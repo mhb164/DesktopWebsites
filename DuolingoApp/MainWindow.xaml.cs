bd29afd5308fd3194523f05dc7f369d9a0cb593e
mhb164 <mhb164@gmail.com>
2026-05-26 00:32:37 +0200

    Add project files.

---
+++ DuolingoApp/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System.Reflection;
+using System.Windows;
+
+namespace DuolingoApp;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private string? Version = Assembly.GetEntryAssembly()?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+    private readonly string _app = "Duolingo";
+    private readonly string _homeUrl = "https://www.duolingo.com";
+
+    public MainWindow()
+    {
+        InitializeComponent(); 
+        InitializeAsync();
+        Title = $"{_app} Wrapper v{Version}";
+    }
+
+    private async void InitializeAsync()
+    {
+        string userDataFolder = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), $"WebsiteWrapperData",_app);
+        var environment = await CoreWebView2Environment.CreateAsync(
+            null,
+            userDataFolder
+        );
+
+        await WebView.EnsureCoreWebView2Async(environment);
+
+        WebView.CoreWebView2.Settings.AreDefaultContextMenusEnabled = false;
+        WebView.CoreWebView2.Settings.AreDevToolsEnabled = false;
+
+        // Show loading
+        LoadingOverlay.Visibility = Visibility.Visible;
+
+        // Page loaded
+        WebView.CoreWebView2.NavigationCompleted += (s, e) =>
+        {
+            LoadingOverlay.Visibility = Visibility.Collapsed;
+        };
+
+        // Restrict navigation
+        WebView.CoreWebView2.NavigationStarting += (s, e) =>
+        {
+            if (!e.Uri.StartsWith(_homeUrl))
+            {
+                e.Cancel = true;
+            }
+
+            LoadingOverlay.Visibility = Visibility.Visible;
+        };
+
+        WebView.CoreWebView2.Navigate(_homeUrl);
+    }
+}
