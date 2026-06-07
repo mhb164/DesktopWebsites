34dae000f78b72134097b6f9ae19855b09fbe0ed
mhb164 <mhb164@gmail.com>
2026-06-07 18:02:32 +0200

    Add SchuleInfoportal1 (https://schueler.schule-infoportal.de)

---
+++ SchuleInfoportal1Desktop/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SchuleInfoportal1Desktop;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
---
+++ SchuleInfoportal1Desktop/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System.Reflection;
+using System.Windows;
+
+namespace SchuleInfoportal1Desktop;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private string? Version = Assembly.GetEntryAssembly()?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+    private readonly string _app = "SchuleInfoportal1";
+    private readonly string _homeUrl = "https://schueler.schule-infoportal.de";
+    public MainWindow()
+    {
+        InitializeComponent();
+        InitializeAsync();
+
+        Title = $"SchuleInfoportal1 App Wrapper v{Version}";
+
+    }
+
+    private async void InitializeAsync()
+    {
+        string userDataFolder = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), $"WebsiteWrapperData",_app);
+        var environment = await CoreWebView2Environment.CreateAsync(
+            null,
+            userDataFolder
+        );
+
+        await WebView.EnsureCoreWebView2Async(environment);
+
+        WebView.CoreWebView2.Settings.AreDefaultContextMenusEnabled = false;
+        WebView.CoreWebView2.Settings.AreDevToolsEnabled = false;
+
+        // Show loading
+        LoadingOverlay.Visibility = Visibility.Visible;
+
+        // Page loaded
+        WebView.CoreWebView2.NavigationCompleted += (s, e) =>
+        {
+            LoadingOverlay.Visibility = Visibility.Collapsed;
+        };
+
+        // Restrict navigation
+        WebView.CoreWebView2.NavigationStarting += (s, e) =>
+        {
+            if (!e.Uri.StartsWith(_homeUrl))
+            {
+                e.Cancel = true;
+            }
+
+            LoadingOverlay.Visibility = Visibility.Visible;
+        };
+
+        WebView.CoreWebView2.Navigate(_homeUrl);
+    }
+}
